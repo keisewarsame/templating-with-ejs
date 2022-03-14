ba6fb93d76a570bd3afad9ca27fb24453160225d
Keise Warsame <91564609+keisewarsame@users.noreply.github.com>
2022-03-14 17:50:56 +0000

    adds different files and pages

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  var title = 'My Home Page';
+  res.render('pages/index',{title:title});
+});
+
+app.get('/dogs', (req, res) => {
+  var title = 'Dogs';
+  res.render('pages/dogs',{title:title});
+});
+
+app.get('/cats', (req, res) => {
+  var title = 'Cats';
+  res.render('pages/cats',{title:title});
+});
+
+app.get('/rabbits', (req, res) => {
+  var title = 'Rabbits';
+  res.render('pages/rabbits',{title:title});
+});
+
+
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}');
+});
