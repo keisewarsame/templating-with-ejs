10e00cb9b2bcb4c13e5b2a981fa5c5b53aab7c0f
Keise Warsame <91564609+keisewarsame@users.noreply.github.com>
2022-03-15 18:25:49 +0000

    Adds list views

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//here is our test data
+var data = require('./data/test.json');
+
 app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -27,7 +30,27 @@ app.get('/rabbits', (req, res) => {
   res.render('pages/rabbits',{title:title});
 });
 
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
 
 app.listen(port, () => {
   console.log('Example app listening on port ${port}');
+  console.log(data);
 });
